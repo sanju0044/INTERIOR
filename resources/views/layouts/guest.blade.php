4bf790df080b4101919219a96df7aa52c5ae6559
sanjay singh <sanjayparihar@learningspiral.co.in>
2025-09-19 22:36:29 +0530

    qwer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
           <script src="{{ asset('js/app.js') }}" defer></script>
 
