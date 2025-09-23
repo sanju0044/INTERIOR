a55c95e00fdf266a9bbc4bda65e176a0e89756f3
sanjay singh <sanjayparihar@learningspiral.co.in>
2025-09-23 20:56:48 +0530

    dddddd

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,59 +1,66 @@
  <!-- Header Container
         ================================================== -->
-        <header id="header-container" class="header head-tr">
-            <!-- Header -->
-            <div id="header" class="head-tr bottom">
-                <div class="container">
-                    <div class="d-flex align-items-center justify-content-between">
+      <header id="header-container" class="header head-tr">
+  <div id="header" class="head-tr bottom">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light p-0">
 
-                    <!-- Logo -->
-                    <div id="logo" class="logo-white">
-                        <a href="index.html">
-                        <img src="{{ asset('images/img/logo.png') }}" data-sticky-logo="{{ asset('images/img/logo.png') }}" alt="Logo">
-                        </a>
-                    </div>
+        <!-- Logo -->
+        <a class="navbar-brand" href="/">
+          <img src="{{ asset('images/img/logo.png') }}"
+               alt="Logo"
+               class="d-inline-block align-top"
+               style="height:50px;">
+        </a>
 
-                    <!-- Navigation -->
-                    <nav id="navigation" class="style-1 head-tr">
-                        <ul id="responsive" class="d-flex list-unstyled mb-0">
-                        <li class="ml-3 font-weight-bold"><a href="/">Home</a></li>
-                        <li class="ml-3 font-weight-bold"><a href="about">About Us</a></li>
-                        <li class="font-weight-bold"><a href="contact.html#">Offering</a>
-                            <ul class="nav-list">
-                                @foreach ($nav as $nv)
-                                    <li>
-                                        <a href="/product-range/{{ encrypt($nv->id) }}">
-                                            <img src="{{ asset($nv->product_img) }}" alt="">
-                                            {{ $nv->product_cat }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </li>
-                        <li class="ml-3 font-weight-bold"><a href="contact">Contact</a></li>
-                        <li class="ml-3 font-weight-bold">
-                            <a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">
-                            GET FREE QUOTE
-                            </a>
-                        </li>
-                        </ul>
-                    </nav>
+        <!-- Mobile Toggle -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-                    <!-- Mobile Navigation -->
-                    <div class="mmenu-trigger d-lg-none">
-                        <button class="hamburger hamburger--collapse" type="button">
-                        <span class="hamburger-box">
-                            <span class="hamburger-inner"></span>
-                        </span>
-                        </button>
-                    </div>
+        <!-- Nav Links -->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav align-items-lg-center">
 
-                    </div>
-                </div>
-                </div>
+            <li class="nav-item ml-lg-3 font-weight-bold">
+              <a class="nav-link" href="/">Home</a>
+            </li>
 
-            <!-- Header / End -->
+            <li class="nav-item ml-lg-3 font-weight-bold">
+              <a class="nav-link" href="/about">About Us</a>
+            </li>
 
-        </header>
+            <!-- Dropdown -->
+            <li class="nav-item dropdown ml-lg-3 font-weight-bold">
+              <a class="nav-link dropdown-toggle" href="#" id="offeringDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Offering
+              </a>
+              <div class="dropdown-menu" aria-labelledby="offeringDropdown">
+                @foreach ($nav as $nv)
+                  <a class="dropdown-item d-flex align-items-center" href="/product-range/{{ encrypt($nv->id) }}">
+                    <img src="{{ asset($nv->product_img) }}" alt="" class="mr-2" style="width:25px; height:25px;">
+                    {{ $nv->product_cat }}
+                  </a>
+                @endforeach
+              </div>
+            </li>
+
+            <li class="nav-item ml-lg-3 font-weight-bold">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+
+            <li class="nav-item ml-lg-3 font-weight-bold">
+              <a class="btn btn-primary text-white px-3 py-1" href="#" data-toggle="modal" data-target="#exampleModal">
+                GET FREE QUOTE
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</header>
+
         <div class="clearfix"></div>
         <!-- Header Container / End -->
